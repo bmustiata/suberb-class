bfbe7621912b943f77d5b20fe60805dcea2b3917
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2015-03-06 12:26:38 +0100

    Added the first test.

--- test/class-inheritance-test.js
+++ test/class-inheritance-test.js
@@ -1,1 +1,12 @@
+var assert = require('assert'),
+    Class = require('../lib/superb-class.js').Class;
 
+describe('Class', function() {
+    describe('calling create', function() {
+        it('should make a function', function() {
+            var MyClass = Class();
+
+            assert.equal("function", typeof MyClass);
+        });
+    });
+});
